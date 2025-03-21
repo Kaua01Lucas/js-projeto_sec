574ac011e56b94f1472f95d5bbea86bb27469b44
Kaua01Lucas <kaua001lucas@gmail.com>
2025-03-20 21:56:36 -0300

    Logica

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-/*let titulo  =  document.querySelector('h1');
-titulo.innerHTML   =  'Jogo de Adivinhação';
-
-let subTitulo  =  document.querySelector('p')
-subTitulo.innerHTML  =  'Escolha um número entre 0 a 100';*/
-
-exibirTextoNaTela('h1', 'Jogo de Adivinhação');
-exibirTextoNaTela('p', "Escolha um número entre 0 a 100")
-
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 100) + 1;
-}
-
-let numeroSecreto = gerarNumeroAleatorio();
-console.log(numeroSecreto);
-=======
 // let titulo = document.querySelector('h1');
 // titulo.innerHTML = 'Jogo de adivinhação';
 
@@ -23,7 +6,12 @@ console.log(numeroSecreto);
 
 exibirTextoNaTela('h1', 'Jogo de adivinhação');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
->>>>>>> ed78fcbe0f71bbd87459d9d06682a62e04fd8bfb
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 100) + 1;
+}
+
+let numeroSecreto = gerarNumeroAleatorio
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -31,13 +19,9 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function verificarChute() {
-    console.log('Apertou o botão');
+    let chute = parseInt(document.querySelector('input')).value;
+    console.log(chute === numeroSecreto);
 }
 
-<<<<<<< HEAD
-
-
-=======
 //<h1>Jogo de adivinhação</h1>
 //<p>Escolha um número entre 1 e 100</p>
->>>>>>> ed78fcbe0f71bbd87459d9d06682a62e04fd8bfb
