6026bb76decea5aacd3c690ff900a1c14757bac8
Kaua01Lucas <kaua001lucas@gmail.com>
2025-03-20 21:31:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ subTitulo.innerHTML  =  'Escolha um número entre 0 a 100';*/
 exibirTextoNaTela('h1', 'Jogo de Adivinhação');
 exibirTextoNaTela('p', "Escolha um número entre 0 a 100")
 
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 100) + 1;
+}
+
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto);
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
